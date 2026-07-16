0eaa1c97641f510932fd8da7c7e6c787433de524
Flavia <flaviavinrodrigues@gmail.com>
2026-07-16 17:33:27 +0530

    port change

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use("/api/register", registerRoute);
 
-const PORT = 5000;
+const PORT = process.env.PORT ||5000;
 
 app.listen(PORT, () => {
   console.log(`✅ Server is running on ${PORT}`);
