92baa22dc2657c7c2ac0f12e862fe07eb89c4534
Flavia <flaviavinrodrigues@gmail.com>
2026-07-16 18:04:10 +0530

    email service change

--- server/config/email.js
+++ server/config/email.js
@@ -1,14 +1,17 @@
 const nodemailer = require("nodemailer");
 
+const nodemailer = require("nodemailer");
+
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // TLS via STARTTLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
 });
+
+module.exports = transporter;
 
 module.exports = transporter;
