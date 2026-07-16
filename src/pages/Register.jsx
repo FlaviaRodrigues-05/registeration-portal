6122c3af218bc61f5e3f5779f3af2e7cb6d24c48
Flavia <flaviavinrodrigues@gmail.com>
2026-07-16 16:57:28 +0530

    Connect frontend to deployed backend

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -28,7 +28,7 @@ function Register() {
 
   try {
     const response = await axios.post(
-      "http://localhost:5000/api/register",
+      "https://registeration-portal.onrender.com",
       formData
     );
 
