dcc6182ab500bed6f04350c1a10c0e623e48941c
Flavia <flaviavinrodrigues@gmail.com>
2026-07-16 16:35:01 +0530

    Fix Render deployment

--- server/config/googleSheets.js
+++ server/config/googleSheets.js
@@ -1,8 +1,12 @@
 const { google } = require("googleapis");
 const path = require("path");
+const fs = require("fs");
+
+const renderSecret = "/etc/secrets/google-service-account.json";
+const localSecret = path.join(__dirname, "ats-portal-502515-8b5ebc28eb1c.json");
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: path.join(__dirname, "ats-portal-502515-8b5ebc28eb1c.json"),
+  keyFile: fs.existsSync(renderSecret) ? renderSecret : localSecret,
   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
 
